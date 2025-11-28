bf3c54060557ddf3891a3c225c4f874e18724560
Johan <johan.furuhjelm@gmail.com>
2025-11-28 09:58:19 +0100

    Matlab script improvements

--- matlab/show_graphs.m
+++ matlab/show_graphs.m
@@ -1,22 +1,24 @@
 %% Start
-load("phri_log.mat")
 dt = 0.002
 time = 0:dt:((length(f) - 1)*dt);
 
 figure(1)
-plot(time, f)
+plot(time, [-f(:, 3), -f(:, 2), -f(:, 1)])
 title("Force")
 xlabel("Time [s]")
 ylabel("Force [N]")
-legend("x", "y", "z", "rx", "ry", "rz")
-
+legend("x", "y", "z")
+%% 
+baserot = atan2(qs(:, 4), qs(:, 3));
+qs_centered = qs - qs(1, :);
+joints = [qs_centered(:, 1), baserot, qs_centered(:, 4:10)];
 figure(2)
-plot(time, qs)
+plot(time, joints)
 title("Joints")
 xlabel("Time [s]")
 ylabel("Joint rot [rad] / Pos [m]")
-legend("Base pos x", "Base pos y", "Base rot x", "Base rot y", "Joint 1", "Joint 2", "Joint 3", "Joint 4", "Joint 5", "Joint 6")
-
+legend("Base pos x", "Base pos y", "Base rot ", "Joint 1", "Joint 2", "Joint 3", "Joint 4", "Joint 5", "Joint 6")
+%% 
 figure(3)
 plot(time, v_cmd)
 title("Velocity commands")
@@ -68,12 +70,19 @@ xlabel("Time [s]")
 ylabel("Value")
 legend("x", "y", "z", "rx", "ry", "rz")
 
+% figure(10)
+% plot(time, [ee_positions, ee_positions_desired])
+% title("Global End effector positions (Task space)")
+% xlabel("Time [s]")
+% ylabel("Value")
+% legend("x", "y", "z", "rx", "ry", "rz", "dx", "dy", "dz", "drx", "dry", "drz")
+
 figure(10)
-plot(time, [ee_positions, ee_positions_desired])
-title("Global End effector positions (Task space)")
+plot(time, ee_positions(:, 1:3) - ee_positions(1, 1:3))
+title("Relative Global End Effector Positions (Task space)")
 xlabel("Time [s]")
-ylabel("Value")
-legend("x", "y", "z", "rx", "ry", "rz", "dx", "dy", "dz", "drx", "dry", "drz")
+ylabel("Distance [m]")
+legend("x", "y", "z", "rx", "ry", "rz")
 
 figure(12)
 plot(time, force_terms)
@@ -82,9 +91,35 @@ xlabel("Time [s]")
 ylabel("Value")
 legend("x", "y", "z", "rx", "ry", "rz")
 
+figure(14)
+plot(time, [-f_2(:, 3), -f_2(:, 2), -f_2(:, 1)])
+title("Force Filtered")
+xlabel("Time [s]")
+ylabel("Force [N]")
+legend("x", "y", "z", "rx", "ry", "rz")
+
+figure(15)
+plot(qs(:, 1), qs(:, 2))
+title("Base trajectory")
+xlabel("X dir")
+ylabel("Y dir")
+plot(ee_positions(:, 1), ee_positions(:, 2))
+axis equal
+legend("Base trajectory", "End effector trajectory")
+
+distance = sqrt((qs(:, 1) - ee_positions(:, 1)).^2 + (qs(:, 2) - ee_positions(:, 2)).^2)
+figure(16)
+hold on
+plot(time, distance)
+yline(0.5)
+title("Distance between end effector and base")
+xlabel("Time [s]")
+ylabel("Distance [m]")
+hold off
+
 %% FFT
 t_start = 0;   % analysis start time (seconds)
-t_end = 2;     % analysis end time (seconds)
+t_end = 6;     % analysis end time (seconds)
 
 % Convert times to sample indices
 i_start = round(t_start / dt) + 1;
@@ -93,7 +128,7 @@ i_end = round(t_end / dt) + 1;
 % Select segment of interest from all force channels
 f_view = f(i_start:i_end, :);
 N = size(f_view, 1);            % Number of time points
-Fs = 1 / dt;               % Sampling frequency (Hz)
+Fs = 1 / dt              % Sampling frequency (Hz)
 freq = (0:N-1) * Fs / N;   % Frequency vector up to Nyquist
 
 % Apply FFT to each channel
@@ -102,11 +137,16 @@ F = fft(f);
 % Plot magnitude spectrum for the first half of frequencies (real signals)
 figure(13)
 
-for k = 1:6
-    subplot(3,2,k);
-    plot(freq(1:floor(N/2)), abs(F(1:floor(N/2),k)));
-    title("FFT of force, between " + t_start + " and " + t_end)
+labels = ["x", "y", "z", "rx", "ry", "rz"]
+
+for k = 1:2
+    subplot(1,2,k);
+    hold on
+    plot([1 1] * 10, ylim)
+    semilogx(freq(1:floor(N/2)), abs(F(1:floor(N/2),k)));
+    title(labels(k) + ": FFT of force, between " + t_start + " and " + t_end)
     xlabel('Frequency (Hz)');
     ylabel('Magnitude');
+    hold off
 end
 
